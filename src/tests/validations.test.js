891ecd3f73fc7bbcbf05bbe8f17bba144f0a8073
Federico Jure <fjure@atixlabs.com>
2019-03-12 10:42:36 -0300

    add project name validation,date validation with tests

--- src/tests/validations.test.js
+++ src/tests/validations.test.js
@@ -1,30 +1,75 @@
-const { assert } = require("chai");
-const { projectValidation } = require("../rest/services/validations/csvImporterValidations");
+const {
+  projectValidation
+} = require("../rest/services/validations/projectValidation");
+
+let mockProjectDto = {
+  projectName: "a name",
+  mission: "a mission",
+  problemAddressed: "a problem",
+  location: "here",
+  timeframe: "15/12//2018",
+  row: 5
+};
+let mockProjectDao = {
+  availableName: jest.fn(name => {
+    return true;
+  })
+};
+
+afterEach(() => {
+  mockProjectDto = {
+    projectName: "a name",
+    mission: "a mission",
+    problemAddressed: "a problem",
+    location: "here",
+    timeframe: "15/12//2018",
+    row: 5
+  };
+  mockProjectDao = {
+    availableName: jest.fn(name => {
+      return true;
+    })
+  };
+});
 
 describe("Project validations tests", () => {
-  it("a project with empty fields must add an error for each empty fields", () => {
-    const mockProjectDto = {
+  test("a project with empty fields must add an error for each empty fields", () => {
+    mockProjectDto = {
       projectName: "  ",
       mission: "  ",
       problemAddressed: "a problem",
       location: "here",
-      timeframe: new Date("1/1/1998").toDateString(),
+      timeframe: "1/1/1998",
       row: 5
     };
-    projectValidation(mockProjectDto);
-    assert.isArray(mockProjectDto.errors);
-    assert.equal(mockProjectDto.errors.length, 2);
+    projectValidation(mockProjectDto, mockProjectDao);
+    expect.arrayContaining(mockProjectDto.errors);
+    expect(mockProjectDto.errors.length).toBe(2);
   });
 
-  it("project timeframe with incorrect date format must add an error", () => {
-    const mockProjectDto = {
+  test("project timeframe with incorrect date format must add an error", () => {
+    mockProjectDto = {
       projectName: "a name",
       mission: "a mission",
       problemAddressed: "a problem",
       location: "here",
-      timeframe: "15/12/20?8",
+      timeframe: "15/12//20?8",
       row: 5
     };
-    projectValidation(mockProjectDto);
+    projectValidation(mockProjectDto, mockProjectDao);
+    expect.arrayContaining(mockProjectDto.errors);
+    expect(mockProjectDto.errors.length).toBe(1);
+  });
+
+  test("project name with existent name must add an error", () => {
+    mockProjectDao = {
+      availableName: jest.fn(name => {
+        throw "Project name already in use";
+      })
+    };
+
+    projectValidation(mockProjectDto, mockProjectDao);
+
+    expect(mockProjectDao.availableName.mock.calls.length).toBe(1);
   });
 });
