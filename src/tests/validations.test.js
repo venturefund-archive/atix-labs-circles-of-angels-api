a3e846ee3188617c0fe23dc07683e0f92a2c63bd
Federico Jure <fjure@atixlabs.com>
2019-03-12 11:58:59 -0300

    Add quarter validation for milestone dto

--- src/tests/validations.test.js
+++ src/tests/validations.test.js
@@ -1,13 +1,16 @@
 const {
   projectValidation
 } = require("../rest/services/validations/projectValidation");
+const {
+  milestoneValidation
+} = require("../rest/services/validations/milestoneValidation");
 
 let mockProjectDto = {
   projectName: "a name",
   mission: "a mission",
   problemAddressed: "a problem",
   location: "here",
-  timeframe: "15/12//2018",
+  timeframe: "12/12/2018",
   row: 5
 };
 let mockProjectDao = {
@@ -15,33 +18,38 @@ let mockProjectDao = {
     return true;
   })
 };
-
-afterEach(() => {
-  mockProjectDto = {
-    projectName: "a name",
-    mission: "a mission",
-    problemAddressed: "a problem",
-    location: "here",
-    timeframe: "15/12//2018",
-    row: 5
-  };
-  mockProjectDao = {
-    availableName: jest.fn(name => {
-      return true;
-    })
-  };
-});
+let mockMilestoneDto = {
+  quarter: "Quarter 1",
+  tasks: "A tasks",
+  impact: "Impact",
+  impactCriterion: "Criterion",
+  signsOfSuccess: "Signs of Success",
+  signsOfSuccessCriterion: "Signs of Success Criterion",
+  category: "Category",
+  keyPersonnel: "224412",
+  budget: "a budget"
+};
 
 describe("Project validations tests", () => {
-  test("a project with empty fields must add an error for each empty fields", () => {
+  afterEach(() => {
     mockProjectDto = {
-      projectName: "  ",
-      mission: "  ",
+      projectName: "a name",
+      mission: "a mission",
       problemAddressed: "a problem",
       location: "here",
-      timeframe: "1/1/1998",
+      timeframe: "12/12/2018",
       row: 5
     };
+    mockProjectDao = {
+      availableName: jest.fn(name => {
+        return true;
+      })
+    };
+  });
+
+  test("a project with empty fields must add an error for each empty fields", () => {
+    mockProjectDto.projectName = " ";
+    mockProjectDto.mission = " ";
     projectValidation(mockProjectDto, mockProjectDao);
     expect.arrayContaining(mockProjectDto.errors);
     expect(mockProjectDto.errors.length).toBe(2);
@@ -53,7 +61,7 @@ describe("Project validations tests", () => {
       mission: "a mission",
       problemAddressed: "a problem",
       location: "here",
-      timeframe: "15/12//20?8",
+      timeframe: "15/12/20?8",
       row: 5
     };
     projectValidation(mockProjectDto, mockProjectDao);
@@ -71,5 +79,29 @@ describe("Project validations tests", () => {
     projectValidation(mockProjectDto, mockProjectDao);
 
     expect(mockProjectDao.availableName.mock.calls.length).toBe(1);
+  });
+});
+
+describe("Milestone dto validations tests", () => {
+  afterEach(() => {
+    mockMilestoneDto = {
+      quarter: "Quarter 1",
+      tasks: "A tasks",
+      impact: "Impact",
+      impactCriterion: "Criterion",
+      signsOfSuccess: "Signs of Success",
+      signsOfSuccessCriterion: "Signs of Success Criterion",
+      category: "Category",
+      keyPersonnel: "224412",
+      budget: "a budget"
+    };
+  });
+
+  test("Quarter out of range must add an error", () => {
+    mockMilestoneDto.quarter = "Quarter 5";
+    milestoneValidation(mockMilestoneDto);
+
+    expect.arrayContaining(mockMilestoneDto.errors);
+    expect(mockMilestoneDto.errors.length).toBe(1);
   });
 });
