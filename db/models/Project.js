b0852461e670552ed5bb7772f5bd022f4b90d934
Federico Jure <fjure@atixlabs.com>
2019-03-15 15:55:00 -0300

    crete user_project table and edit project table script

--- db/models/Project.js
+++ db/models/Project.js
@@ -11,6 +11,9 @@ module.exports = {
     coverPhoto: { type: 'string', required: true },
     cardPhoto: { type: 'string', required: true },
     status: { type: 'number', defaultsTo: 0 },
+    goalAmount: { type: 'number', required: true },
+    faqLink: { type: 'string', required: true },
+    pitchProposal: { type: 'string', required: true },
     createdAt: { type: 'string', autoCreatedAt: true, required: false },
     updatedAt: { type: 'string', autoUpdatedAt: true, required: false },
     milestones: {
--- db/scripts/006-13-03-2019-create-project-table.sql
+++ db/scripts/006-13-03-2019-create-project-table.sql
@@ -9,6 +9,9 @@ CREATE TABLE public.project (
 	"coverPhoto" text not null,
 	"cardPhoto" text not null,
 	"status" int2 not null,
+	"goalAmount" int8 not null,
+	"faqLink" varchar not null,
+	"pitchProposal" varchar(100) not null,
 	"createdAt" date,
-	"updatedAt" date
+	"updatedAt" date 
 );
