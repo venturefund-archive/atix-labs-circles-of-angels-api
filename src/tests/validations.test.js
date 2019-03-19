2eb224e3643ef4937324b689f8e51503169ab22b
Federico Jure <fjure@atixlabs.com>
2019-03-19 17:49:33 +0000

    Add dtos validations

---
+++ src/tests/validations.test.js
@@ -0,0 +1,48 @@
+jest.mock("../rest/dao/projectDao");
+
+describe("Project validations tests", () => {
+  const projectValidation = require("../rest/services/validations/projectValidation");
+  let mockProjectDto;
+  let projectDao;
+
+  beforeEach(() => {
+    
+    projectDao = require("../rest/dao/projectDao");
+    mockProjectDto = {
+      projectName: "a name",
+      mission: "a mission",
+      problemAddressed: "a problem",
+      location: "here",
+      timeframe: "11/12/2018",
+      row: 5
+    };
+    projectDao.availableName = jest.fn(name => {
+      return true;
+    });
+  });
+
+  test("a project with empty fields must add an error for each empty fields", () => {
+    mockProjectDto.projectName = " ";
+    mockProjectDto.mission = " ";
+    projectValidation(mockProjectDto);
+    expect.arrayContaining(mockProjectDto.errors);
+    expect(mockProjectDto.errors.length).toBe(2);
+  });
+
+  test("project timeframe with incorrect date format must add an error", () => {
+    mockProjectDto.timeframe = " 11/12/20?8";
+    projectValidation(mockProjectDto);
+    expect.arrayContaining(mockProjectDto.errors);
+    expect(mockProjectDto.errors.length).toBe(1);
+  });
+
+  test("project name with existent name must add an error", () => {
+    projectDao.availableName = jest.fn(name => {
+      throw "Project name already in use";
+    });
+
+    projectValidation(mockProjectDto);
+
+    expect(projectDao.availableName.mock.calls.length).toBe(1);
+  });
+});
