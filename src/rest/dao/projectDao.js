ce7f4847a96b883c90315e6afb0b3fc9221f6e2f
Federico Jure <fjure@atixlabs.com>
2019-03-19 18:08:35 +0000

    Merge branch 'develop' into 'COA-268-Endpoint-to-get-one-project-with-details'
    # Conflicts:
    #   src/rest/core/projectService.js
    #   src/rest/dao/projectDao.js
    #   src/rest/routes/projectRoutes.js
    #   src/tests/projectService.test.js

--- src/rest/dao/projectDao.js
+++ src/rest/dao/projectDao.js
@@ -1,11 +1,29 @@
-const ProjectDao = ({ projectModel }) => ({
+const _ = require('lodash');
+
+const ProjectDao = ({ projectModel, userDao }) => ({
   async saveProject(project) {
     const createdProject = await projectModel.create(project);
     return createdProject;
   },
   async getProjecListWithStatusFrom({ status }) {
-    const projects = projectModel.find({ where: { status: { '>=': status } } });
+    let projects = await projectModel.find({
+      where: { status: { '>=': status } }
+    });
+    for (var i in projects) {
+      const aux = await this.addUserInfoOnProject({ project: projects[i] });
+      projects[i] = aux;
+    }
     return projects;
+  },
+  async getProjectById({ projectId }) {
+    const project = await projectModel.findOne({ id: projectId });
+    return this.addUserInfoOnProject({ project });
+  },
+  async addUserInfoOnProject({ project }) {
+    const user = await userDao.getUserById({ id: project.ownerId });
+    project.ownerName = user.username;
+    project.ownerEmail = user.email;
+    return project;
   }
 });
 
--- src/rest/routes/projectRoutes.js
+++ src/rest/routes/projectRoutes.js
@@ -1,32 +1,87 @@
 const fileUpload = require('fastify-file-upload');
 
 const basePath = '/project';
-
 const routes = async fastify => {
   fastify.register(fileUpload);
-  const activityDao = require('../dao/activityDao')({
-    activityModel: fastify.models.activity
-  });
+
+  const activityDao = require('../dao/activityDao')(fastify.models.activity);
   const activityService = require('../core/activityService')({
     fastify,
     activityDao
   });
-  const milestoneDao = require('../dao/milestoneDao')({
-    milestoneModel: fastify.models.project
-  });
+  const milestoneDao = require('../dao/milestoneDao')(fastify.models.milestone);
   const milestoneService = require('../core/milestoneService')({
     fastify,
     milestoneDao,
     activityService
   });
+  const userDao = require('../dao/userDao')({ userModel: fastify.models.user });
   const projectDao = require('../dao/projectDao')({
-    projectModel: fastify.models.project
+    projectModel: fastify.models.project,
+    userDao
   });
   const projectService = require('../core/projectService')({
     fastify,
     projectDao,
     milestoneService
   });
+
+  fastify.post(
+    `${basePath}/create`,
+    {
+      schema: {
+        type: 'multipart/form-data',
+        raw: {
+          files: { type: 'object' },
+          body: { type: 'object' }
+        }
+      },
+      response: {
+        200: {
+          type: 'object',
+          properties: {
+            response: { type: 'object' }
+          }
+        }
+      }
+    },
+    async (req, reply) => {
+      fastify.log.info(
+        '[Project Routes] :: POST request at /project/upload:',
+        req.raw.body,
+        req.raw.files
+      );
+
+      const {
+        projectProposal,
+        projectCoverPhoto,
+        projectCardPhoto,
+        projectMilestones
+      } = req.raw.files;
+
+      const { project } = req.raw.body;
+
+      try {
+        await projectService.createProject(
+          project,
+          projectProposal,
+          projectCoverPhoto,
+          projectCardPhoto,
+          projectMilestones
+        );
+      } catch (error) {
+        fastify.log.error(
+          '[Project Routes] :: Error creating project: ',
+          error
+        );
+        reply.status(500).send({ error: 'Error creating project' });
+      }
+
+      reply.send({ sucess: 'Project created successfully!' });
+    }
+  );
+
+  // ** NOT USED **
   fastify.post(
     `${basePath}/upload`,
     {
@@ -59,7 +114,7 @@ const routes = async fastify => {
       } = req.raw.files;
 
       try {
-        await projectService.createProject(
+        await projectService.createProjectWithFile(
           projectXls,
           projectCoverPhoto,
           projectCardPhoto,
@@ -90,7 +145,7 @@ const routes = async fastify => {
       }
     },
     async (request, reply) => {
-      fastify.log.info(`[Project Routes] :: Getting projects`);
+      fastify.log.info('[Project Routes] :: Getting projects');
       try {
         //When User role verification implemented -> if Backoffice admin, get all projects, else get active projects
         const projects = await projectService.getProjectList();
@@ -99,6 +154,36 @@ const routes = async fastify => {
         fastify.log.error(error);
         reply.status(500).send({ error: 'Error getting projects' });
       }
+    }
+  );
+
+  fastify.get(
+    `${basePath}/:projectId/getProject`,
+    {
+      schema: {
+        params: {
+          projectId: { type: 'integer' }
+        }
+      },
+      response: {
+        200: {
+          type: 'object',
+          properties: {
+            response: { type: 'object' }
+          }
+        }
+      }
+    },
+    async (request, reply) => {
+      const { projectId } = request.params;
+      fastify.log.info(
+        `[Project Routes] :: Getting project with id ${projectId}`
+      );
+
+      const project = await projectService.getProjectWithId({
+        projectId
+      });
+      reply.send(project);
     }
   );
 };
