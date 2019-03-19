efb9a84fc9c945c3807f88b093bef6be0d329da9
Jonathan Vera <jvera@atixlabs.com>
2019-03-19 20:03:30 +0000

    273- add UserProject

--- db/models/User.js
+++ db/models/User.js
@@ -9,5 +9,8 @@ module.exports = {
     createdAt: { type: 'string', autoCreatedAt: true, required: false },
     updatedAt: { type: 'string', autoUpdatedAt: true, required: false },
     id: { type: 'number', autoMigrations: { autoIncrement: true } },
+  },
+  findById: async function(id) {
+    return this.findOne(id);
   }
 };
