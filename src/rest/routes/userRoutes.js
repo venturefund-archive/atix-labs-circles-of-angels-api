b718192059a661a6d5bbce1ad51f1450f4fef323
Leo Gobbi <gobbileonel@gmail.com>
2019-03-20 11:59:00 -0300

    COA-284 remove userId field from User model

--- src/rest/routes/userRoutes.js
+++ src/rest/routes/userRoutes.js
@@ -37,7 +37,7 @@ const routes = async (fastify, options) => {
       reply.send({
         name: user.username,
         email: user.email,
-        userid: user.userid
+        userid: user.id
       });
     }
   );
