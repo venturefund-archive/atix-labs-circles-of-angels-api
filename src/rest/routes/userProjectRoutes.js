1af985ce186a2e5d928af932535742260e2497ad
Leonel Gobbi <lgobbi@atixlabs.com>
2019-03-26 19:42:08 +0000

    Coa 319 associate user project

--- src/rest/routes/userProjectRoutes.js
+++ src/rest/routes/userProjectRoutes.js
@@ -75,6 +75,65 @@ const routes = async fastify => {
       }
     }
   );
+
+  fastify.get(
+    `${basePath}/:userId/:projectId/create`,
+    {
+      schema: {
+        params: {
+          userId: { type: 'integer' },
+          projectId: { type: 'integer' }
+        }
+      },
+      response: {
+        200: {
+          type: 'object',
+          properties: {
+            response: { type: 'object' }
+          }
+        }
+      }
+    },
+    async (request, reply) => {
+      const { userId, projectId } = request.params;
+
+      fastify.log.info(
+        `[User Project Routes] :: Associating User ID ${userId} to Project ID 
+        ${projectId}`
+      );
+
+      try {
+        const userProject = await userProjectService.createUserProject(
+          userId,
+          projectId
+        );
+
+        if (userProject.error) {
+          fastify.log.error(
+            '[User Project Routes] :: Error creating user-project relation: ',
+            userProject.error
+          );
+          reply.status(userProject.status).send(userProject.error);
+        } else {
+          fastify.log.info(
+            '[User Routes Service] :: User-Project relation created succesfully: ',
+            userProject
+          );
+          reply
+            .status(200)
+            .send({ success: 'User-project relation created successfully!' });
+        }
+      } catch (error) {
+        fastify.log.error(
+          '[User Project Routes] :: Error creating user-project relation: ',
+          error
+        );
+        reply
+          .status(500)
+          .send({ error: 'Error creating user-project relation' });
+      }
+    }
+  );
 };
 
 module.exports = routes;
