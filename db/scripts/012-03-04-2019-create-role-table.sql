10ef8d22697428235b93883a91a4a79f8fd67693
Leo Gobbi <gobbileonel@gmail.com>
2019-04-03 10:52:15 -0300

    COA-382 add database scripts

---
+++ db/scripts/012-03-04-2019-create-role-table.sql
@@ -0,0 +1,4 @@
+CREATE TABLE public."role" (
+    "id" SERIAL primary key,
+    "name" varchar(50) NOT null
+);
---
+++ db/scripts/013-03-04-2019-insert-into-role.sql
@@ -0,0 +1,15 @@
+INSERT INTO public."role"
+("name")
+VALUES('BO Admin');
+
+INSERT INTO public."role"
+("name")
+VALUES('Social Entrepreneur');
+
+INSERT INTO public."role"
+("name")
+VALUES('Funder');
+
+INSERT INTO public."role"
+("name")
+VALUES('Oracle');
