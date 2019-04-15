ce373cce47fe6cedc5ed03a181924ffee8fe5ac2
Leo Gobbi <gobbileonel@gmail.com>
2019-04-15 09:08:55 -0300

    COA-443 start project endpoint

--- src/rest/util/constants.js
+++ src/rest/util/constants.js
@@ -4,13 +4,21 @@ const evidenceFileTypes = {
 };
 
 const transferStatus = {
-  RECONSILIATION: 'Reconsiliaton',
-  PENDING_VERIFICATION: 'Pending Verification',
-  CANCELLED: 'Cancelled',
-  VERIFIED: 'Verified'
+  CANCELLED: { status: -1, name: 'Cancelled' },
+  PENDING_VERIFICATION: { status: 0, name: 'Pending Verification' },
+  RECONSILIATION: { status: 1, name: 'Reconsiliaton' },
+  VERIFIED: { status: 2, name: 'Verified' }
+};
+
+const projectStatus = {
+  REJECTED: { status: -1, name: 'Rejected' },
+  PENDING_APPROVAL: { status: 0, name: 'Pending Approval' },
+  PUBLISHED: { status: 1, name: 'Published' },
+  IN_PROGRESS: { status: 2, name: 'In Progress' }
 };
 
 module.exports = {
   evidenceFileTypes,
-  transferStatus
+  transferStatus,
+  projectStatus
 };
