0905535a6be1ae7556559e479de728d15efe4952
Leonel Gobbi <lgobbi@atixlabs.com>
2019-04-25 19:26:19 +0000

    COA-532 add hash to evidences

--- db/scripts/017-08-04-2019-create-activity_photo-table.sql
+++ db/scripts/017-08-04-2019-create-activity_photo-table.sql
@@ -2,6 +2,7 @@ CREATE TABLE public.activity_photo (
 	"id" SERIAL PRIMARY KEY,
 	"activityId" int4 NOT NULL,
 	"photoId" int4 NOT NULL,
+	"fileHash" varchar(80) NULL,
 	"createdAt" date NULL,
 	"updatedAt" date NULL,
 	FOREIGN KEY ("activityId") REFERENCES activity(id) on delete cascade,
