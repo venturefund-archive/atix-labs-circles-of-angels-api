62b96675e923297103fa0f4ea5b8eb126e7b142e
Federico Jure <fjure@atixlabs.com>
2019-04-26 11:36:00 -0300

    fix scripts and add script to download flyway

--- db/scripts/016-08-04-2019-create-activity_file-table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE public.activity_file (
-	"id" SERIAL PRIMARY KEY,
-	"activityId" int4 NOT NULL,
-	"fileId" int4 NOT NULL,
-	"createdAt" date NULL,
-	"updatedAt" date NULL,
-	FOREIGN KEY ("activityId") REFERENCES activity(id) on delete cascade,
-	FOREIGN KEY ("fileId") REFERENCES file(id) on delete cascade
-);
