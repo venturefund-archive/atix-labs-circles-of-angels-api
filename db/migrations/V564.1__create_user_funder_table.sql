813a120970411c6d4f811271c910c7bb69b52531
Federico Jure <fjure@atixlabs.com>
2019-04-30 14:50:36 -0300

    COA-564 save detail info for SE on register

--- db/migrations/V564.1__create_user_funder_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE public.user_funder (
-	"id" SERIAL primary key,
-	"userId" int4 not null,
-	"identifier" varchar(80) not null,
-	"address" varchar(80) not null,
-	"tel" varchar(40) not null,
-	foreign key ("userId") references "user"(id)
-);
