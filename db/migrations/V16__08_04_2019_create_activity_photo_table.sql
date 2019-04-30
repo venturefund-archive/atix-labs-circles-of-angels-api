36d09745a5b684913d39ef1583ecabdcebb95198
Leo Gobbi <gobbileonel@gmail.com>
2019-04-30 10:30:06 -0300

    COA-561 create table user_registration_status and alter user table

--- db/migrations/V16__08_04_2019_create_activity_photo_table.sql
+++ db/migrations/V16__08_04_2019_create_activity_photo_table.sql
@@ -2,7 +2,6 @@ CREATE TABLE public.activity_photo (
 	"id" SERIAL PRIMARY KEY,
 	"activityId" int4 NOT NULL,
 	"photoId" int4 NOT NULL,
-	"fileHash" varchar(80) NULL,
 	"createdAt" date NULL,
 	"updatedAt" date NULL,
 	"fileHash" varchar(80) NULL,
--- db/migrations/V17__08_04_2019_create_activity_file_table.sql
+++ db/migrations/V17__08_04_2019_create_activity_file_table.sql
@@ -2,7 +2,6 @@ CREATE TABLE public.activity_file (
 	"id" SERIAL PRIMARY KEY,
 	"activityId" int4 NOT NULL,
 	"fileId" int4 NOT NULL,
-	"fileHash" varchar(80) NULL,
 	"createdAt" date NULL,
 	"updatedAt" date NULL,
 	"fileHash" varchar(80) NULL,
