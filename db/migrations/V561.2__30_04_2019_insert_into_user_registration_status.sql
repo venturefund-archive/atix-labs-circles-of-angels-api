aa69f40ada968df0bce5728c0e04c84a5e6f85b1
Leo Gobbi <gobbileonel@gmail.com>
2019-04-30 11:49:22 -0300

    COA-561 fix flyway scripts version with card ID

---
+++ db/migrations/V561.2__30_04_2019_insert_into_user_registration_status.sql
@@ -0,0 +1,3 @@
+INSERT INTO user_registration_status VALUES (1, 'Pending Approval');
+INSERT INTO user_registration_status VALUES (2, 'Approved');
+INSERT INTO user_registration_status VALUES (3, 'Rejected');
