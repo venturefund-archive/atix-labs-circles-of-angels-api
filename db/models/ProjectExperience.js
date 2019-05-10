9005dca65d457e0898b850c9ea9a6cb11a49c7db
lgobbi <lgobbi@atixlabs.com>
2019-05-10 11:50:30 -0300

    COA-598 create project_experience table and model

---
+++ db/models/ProjectExperience.js
@@ -0,0 +1,31 @@
+/**
+ * @description Represents a COA user experience with a project,
+ * requires a comment and may have an attached file
+ * @attribute `id`: unique identifier
+ * @attribute `project`: reference to the related project
+ * @attribute `user`: reference to the related user
+ * @attribute `file`: reference to the attached file
+ * @attribute `comment`: comments from the user
+ */
+module.exports = {
+  identity: 'project_experience',
+  primaryKey: 'id',
+  attributes: {
+    project: {
+      columnName: 'projectId',
+      model: 'project'
+    },
+    user: {
+      columnName: 'userId',
+      model: 'user'
+    },
+    file: {
+      columnName: 'fileId',
+      model: 'file'
+    },
+    comment: { type: 'string', required: true },
+    createdAt: { type: 'string', autoCreatedAt: true, required: true },
+    updatedAt: { type: 'string', autoUpdatedAt: true, required: false },
+    id: { type: 'number', autoMigrations: { autoIncrement: true } }
+  }
+};
