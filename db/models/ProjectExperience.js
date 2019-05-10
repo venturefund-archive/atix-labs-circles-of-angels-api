39f69fbce5d42661b94af06454c02f73903aa5b2
lgobbi <lgobbi@atixlabs.com>
2019-05-10 11:53:31 -0300

    COA-598 remove required flag from createdAt and updatedAt

--- db/models/ProjectExperience.js
+++ db/models/ProjectExperience.js
@@ -4,11 +4,7 @@
  * @attribute `id`: unique identifier
  * @attribute `project`: reference to the related project
  * @attribute `user`: reference to the related user
-<<<<<<< HEAD
  * @attribute `photos`: reference to the attached files
-=======
- * @attribute `file`: reference to the attached file
->>>>>>> COA-598 create project_experience table and model
  * @attribute `comment`: comments from the user
  */
 module.exports = {
@@ -23,7 +19,6 @@ module.exports = {
       columnName: 'userId',
       model: 'user'
     },
-<<<<<<< HEAD
     photos: {
       collection: 'photo',
       via: 'projectExperience'
@@ -31,15 +26,6 @@ module.exports = {
     comment: { type: 'string', required: true },
     createdAt: { type: 'string', autoCreatedAt: true },
     updatedAt: { type: 'string', autoUpdatedAt: true },
-=======
-    file: {
-      columnName: 'fileId',
-      model: 'file'
-    },
-    comment: { type: 'string', required: true },
-    createdAt: { type: 'string', autoCreatedAt: true, required: true },
-    updatedAt: { type: 'string', autoUpdatedAt: true, required: false },
->>>>>>> COA-598 create project_experience table and model
     id: { type: 'number', autoMigrations: { autoIncrement: true } }
   }
 };
