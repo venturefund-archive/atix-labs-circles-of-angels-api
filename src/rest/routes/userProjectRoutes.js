c68afb98de8ccd074a5ce6c27d3b9614b23ecdd8
lgobbi <lgobbi@atixlabs.com>
2019-06-13 15:21:58 -0300

    COA-677 fix userProject routes schemas

--- src/rest/routes/userProjectRoutes.js
+++ src/rest/routes/userProjectRoutes.js
@@ -20,15 +20,38 @@ const routes = {
           'Sign the agreement of an existing project by an existing funder',
         summary: 'Funder sign project agreement',
         params: {
-          userId: { type: 'integer' },
-          projectId: { type: 'integer' }
-        }
-      },
-      response: {
-        200: {
           type: 'object',
           properties: {
-            response: { type: 'object' }
+            userId: {
+              type: 'integer',
+              description: 'User that will be signing the agreement'
+            },
+            projectId: {
+              type: 'integer',
+              description: 'Project to which the agreement document belongs to'
+            }
+          }
+        },
+        response: {
+          200: {
+            description: 'Returns the record that was modified',
+            type: 'array',
+            items: {
+              type: 'object',
+              properties: {
+                status: { type: 'integer' },
+                id: { type: 'integer' },
+                user: { type: 'integer' },
+                project: { type: 'integer' }
+              }
+            }
+          },
+          '4xx': {
+            description: 'Returns a message describing the error',
+            type: 'object',
+            properties: {
+              error: { type: 'string' }
+            }
           }
         }
       }
@@ -45,14 +68,46 @@ const routes = {
         description: 'Returns all funders related to a project',
         summary: 'Get all funders by project',
         params: {
-          projectId: { type: 'integer' }
-        }
-      },
-      response: {
-        200: {
           type: 'object',
           properties: {
-            response: { type: 'object' }
+            projectId: {
+              description: 'Project to get the users from',
+              type: 'integer'
+            }
+          }
+        },
+        response: {
+          200: {
+            type: 'array',
+            description: 'Returns a list of user objects',
+            items: {
+              type: 'object',
+              properties: {
+                status: { type: 'integer' },
+                id: { type: 'integer' },
+                project: { type: 'integer' },
+                user: {
+                  type: 'object',
+                  properties: {
+                    username: { type: 'string' },
+                    email: { type: 'string' },
+                    address: { type: 'string' },
+                    createdAt: { type: 'string' },
+                    updatedAt: { type: 'string' },
+                    id: { type: 'integer' },
+                    role: { type: 'integer' },
+                    registrationStatus: { type: 'integer' }
+                  }
+                }
+              }
+            }
+          },
+          '4xx': {
+            type: 'object',
+            description: 'Returns a message describing the error',
+            properties: {
+              error: { type: 'string' }
+            }
           }
         }
       }
@@ -70,15 +125,33 @@ const routes = {
           'Creates a new relation between an existing funder and an existing project',
         summary: 'Associate a funder to a project',
         params: {
-          userId: { type: 'integer' },
-          projectId: { type: 'integer' }
-        }
-      },
-      response: {
-        200: {
           type: 'object',
           properties: {
-            response: { type: 'object' }
+            userId: { type: 'integer' },
+            projectId: { type: 'integer' }
+          }
+        },
+        response: {
+          200: {
+            type: 'object',
+            description: 'Success message if the relation was created',
+            properties: {
+              success: { type: 'string' }
+            }
+          },
+          '4xx': {
+            type: 'object',
+            description: 'Returns a message describing the error',
+            properties: {
+              error: { type: 'string' }
+            }
+          },
+          500: {
+            type: 'object',
+            description: 'Returns a message describing the error',
+            properties: {
+              error: { type: 'string' }
+            }
           }
         }
       }
