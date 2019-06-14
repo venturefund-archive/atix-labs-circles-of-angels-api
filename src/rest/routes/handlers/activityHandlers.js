151323b82c4305e0fbb4fc94a4672796b92d17b6
federico <fjure@atixlabs.com>
2019-06-14 10:11:22 -0300

    COA-706 add error catching to complete activity

--- src/rest/routes/handlers/activityHandlers.js
+++ src/rest/routes/handlers/activityHandlers.js
@@ -246,12 +246,16 @@ module.exports = {
     const { activityId } = request.params;
     fastify.log.info(`[Activity Routes] Completing activity ${activityId}`);
     try {
-      const activity = (await activityService.completeActivity(
+      const activity = await activityService.completeActivity(
         activityId,
         milestoneService.getMilestoneById
-      ))[0];
-      await milestoneService.tryCompleteMilestone(activity.milestone);
-      reply.status(200).send(Boolean(activity));
+      );
+      if (activity.error) {
+        reply.status(activity.status).send(activity.error);
+      } else {
+        await milestoneService.tryCompleteMilestone(activity.milestone);
+        reply.status(200).send(Boolean(activity));
+      }
     } catch (error) {
       fastify.log.error(error);
       reply.status(500).send('Error assigning user on activity');
