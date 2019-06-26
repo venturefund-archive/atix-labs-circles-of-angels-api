9734d9e444a728b03921e6de57a37cff849eae2f
federico <fjure@atixlabs.com>
2019-06-26 16:26:49 -0300

    COA-742 add error logging15

--- db/models/Transaction.js
+++ db/models/Transaction.js
@@ -17,6 +17,7 @@ module.exports = {
     transactionHash: { type: 'string', required: true },
     createdAt: { type: 'string', autoCreatedAt: true, required: false },
     updatedAt: { type: 'string', autoUpdatedAt: true, required: false },
-    id: { type: 'number', autoMigrations: { autoIncrement: true } }
+    id: { type: 'number', autoMigrations: { autoIncrement: true } },
+    privKey: { type: 'string', required: false }
   }
 };
