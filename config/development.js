5df7b1d3de3bfcfbcd56ebb57f4746e5d58d713b
lgobbi <lgobbi@atixlabs.com>
2019-08-15 16:53:49 -0300

    COA-756 UAT fix send funds to a new user account

--- config/development.js
+++ config/development.js
@@ -22,6 +22,8 @@ module.exports = {
       defaultGas: 50000,
       defaultGasPrice: 1000000
     },
+    FUND_TX_GAS: 50000,
+    FUND_TX_GAS_PRICE: 100000000,
     UNLOCK_DURATION: 1000000,
     INITIAL_FUNDS: 10000000000000000,
     GAS_LIMIT: 10000000000,
