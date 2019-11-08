7c36d80a92487305ec7d743fe97c086ad0f79b43
Budi <ignacio.tirabasso@gmail.com>
2019-11-08 15:41:36 -0300

    Remove unused variable

--- src/rest/util/injection.js
+++ src/rest/util/injection.js
@@ -1,4 +1,4 @@
-function getDescriptors(instance, values, mockable = false) {
+function getDescriptors(values, mockable = false) {
   // map property descriptors
   return Object.entries(values).reduce(
     (acc, [name, value]) =>
@@ -12,9 +12,9 @@ function getDescriptors(instance, values, mockable = false) {
 module.exports = {
 
   injectMocks(instance, mocks) {
-    Object.defineProperties(instance, getDescriptors(instance, mocks, true));
+    Object.defineProperties(instance, getDescriptors(mocks, true));
   },
   injectDependencies(instance, dependencies) {
-    Object.defineProperties(instance, getDescriptors(instance, dependencies));
+    Object.defineProperties(instance, getDescriptors(dependencies));
   }
 };
