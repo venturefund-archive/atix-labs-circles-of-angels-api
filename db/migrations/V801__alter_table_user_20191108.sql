d61b6a32f9f3cd27692e833082a5a6fceece62a9
Budi <ignacio.tirabasso@gmail.com>
2019-11-14 17:09:16 -0300

    fix migrations

--- db/migrations/V801__alter_table_user_20191108.sql
+++ db/migrations/V801__alter_table_user_20191108.sql
@@ -1,2 +1,2 @@
-ALTER TABLE user RENAME COLUMN username TO firstname;
-ALTER TABLE user ADD COLUMN lastname NOT NULL DEFAULT '';
+ALTER TABLE public.user RENAME COLUMN username TO firstname;
+ALTER TABLE public.user ADD COLUMN lastname NOT NULL DEFAULT '';
