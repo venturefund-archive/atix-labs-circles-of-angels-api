6c4935d8cbe868e13e0209a28977b1d3c874ab5c
Maira Diaz <mdiaz@atixlabs.com>
2019-11-15 10:04:53 -0300

    rebase with last changes from develop

--- src/rest/ioc.js
+++ src/rest/ioc.js
@@ -13,14 +13,10 @@ const projectService = require('./services/projectService');
 
 const userDao = require('./dao/userDao');
 const projectDao = require('./dao/projectDao');
-<<<<<<< HEAD
 const roleDao = require('./dao/roleDao');
-=======
 const passRecoveryService = require('./services/passRecoveryService');
->>>>>>> initial refactor: inject services and dao
 
 const passRecoveryDao = require('./dao/passRecoveryDao');
-const roleDao = require('./dao/roleDao');
 const { injectDependencies } = require('./util/injection');
 
 module.exports = fastify => {
@@ -88,12 +84,9 @@ module.exports = fastify => {
 
   function configureDAOs(models) {
     injectModel(userDao, models.user);
-<<<<<<< HEAD
     injectModel(roleDao, models.role);
     injectModel(projectDao, models.project);
-=======
     injectModel(passRecoveryDao, models.passRecovery);
->>>>>>> initial refactor: inject services and dao
   }
 
   function configureServices() {
