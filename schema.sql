82c06c4563416da654d8f36c304655466676fbf3
tdallas <tdallas@atixlabs.com>
2019-11-28 10:50:58 -0300

    feature - schema refactor, all ids are serial, add some fk and change proposal to text

--- schema.sql
+++ schema.sql
@@ -1,140 +1,136 @@
-CREATE ROLE atixlabs;
-
 CREATE DATABASE coadb;
+
 ALTER DATABASE coadb OWNER TO postgres;
 
 \connect coadb
-
-CREATE TYPE role AS ENUM ('entrepreneur', 'funder', 'oracle', 'admin');
+CREATE TYPE ROLE AS ENUM (
+    'entrepreneur',
+    'funder',
+    'oracle',
+    'admin'
+);
 
 CREATE TABLE public.user (
-    id int NOT NULL SERIAL,
+    id SERIAL NOT NULL,
     firstName varchar(80) NOT NULL,
-    lastName varchar(80)
+    lastName varchar(80),
     email varchar(40) NOT NULL,
     password varchar(80) NOT NULL,
-    -- TODO : is there any way to use `role` as field name.  
-    roleName role NOT NULL,
+    -- TODO : is there any way to use `role` as field name.
+    roleName ROLE NOT NULL,
     createdAt date DEFAULT now(),
     address varchar(42) NOT NULL,
     privKey varchar(80) NOT NULL,
-    PRIMARY KEY(id),
-    UNIQUE(email)
+    PRIMARY KEY (id),
+    UNIQUE (email)
 );
 
-CREATE TYPE ProjectStatus AS ENUM ('pending', 'consensus', 'ongoing');
+CREATE TYPE ProjectStatus AS ENUM (
+    'editing',
+    'pending',
+    'consensus',
+    'ongoing'
+);
 
 CREATE TABLE public.project (
-    id int NOT NULL,
+    id SERIAL NOT NULL,
     projectName varchar(50) NOT NULL,
     ownerId integer NOT NULL,
     createdAt date DEFAULT NOW(),
     transactionHash varchar(80) NOT NULL,
-
-    mission text, 
+    mission text,
     location text,
     problemAddressed text,
     timeframe text,
-    status ProjectStatus DEFAULT 'pending',
+    status ProjectStatus DEFAULT 'editing',
     goalAmount real NOT NULL,
     faqLink text,
     coverPhotoPath varchar(100),
     cardPhotoPath varchar(100),
-    proposalPath varchar(100),
+    proposal text,
     agreementPath varchar(100),
-
-    PRIMARY KEY(id),
-    FOREIGN KEY(ownerId) REFERENCES public.user(id)
-
+    PRIMARY KEY (id),
+    FOREIGN KEY (ownerId) REFERENCES public.user (id)
 );
 
 CREATE TABLE public.milestone (
-    id int SERIAL,
+    id SERIAL NOT NULL,
     projectId int,
     createdAt date DEFAULT NOW(),
-    description text
+    description text,
     category text,
-    PRIMARY KEY(id),
-    FOREIGN KEY(projectId) REFERENCES public.project(id)
+    PRIMARY KEY (id),
+    FOREIGN KEY (projectId) REFERENCES public.project (id)
 );
-
 
 -- CREATE TABLE public.activity (
 CREATE TABLE public.task (
-    id int SERIAL,
+    id SERIAL NOT NULL,
     milestoneId int,
     createdAt date DEFAULT NOW(),
     taskHash varchar(80) DEFAULT NULL,
-    oracleAddress varchar(42) NOT NULL
+    oracleAddress varchar(42) NOT NULL,
     description text, -- TODO : should be NOT NULL
     reviewCriteria text,
     category text,
     keyPersonnel text,
     budget text,
-    PRIMARY KEY(id),
-    FOREIGN KEY(milestoneId) REFERENCES public.milestone(id)
+    PRIMARY KEY (id),
+    FOREIGN KEY (milestoneId) REFERENCES public.milestone (id),
+    FOREIGN KEY (oracleAddress) REFERENCES public.user (address)
 );
 
 CREATE TABLE public.transaction (
-    id int NOT NULL SERIAL,
+    id SERIAL NOT NULL,
     sender varchar(42) NOT NULL,
     data text NOT NULL,
-    PRIMARY KEY(id)
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE public.project_experience (
-    id int NOT NULL SERIAL,
+    id SERIAL NOT NULL,
     projectId int NOT NULL,
-    userId int NOT NULL, 
+    userId int NOT NULL,
     comment text NOT NULL,
     createdAt timestamp with time zone NOT NULL,
-    PRIMARY KEY(id),
-    FOREIGN KEY(projectId) REFERENCES public.project(id),
-    FOREIGN KEY(userId) REFERENCES public.user(id)
+    PRIMARY KEY (id),
+    FOREIGN KEY (projectId) REFERENCES public.project (id),
+    FOREIGN KEY (userId) REFERENCES public.user (id)
 );
 
 CREATE TABLE public.pass_recovery (
-    id int NOT NULL,
+    id SERIAL NOT NULL,
     token varchar(80) NOT NULL,
     email varchar(80) NOT NULL,
     createdAt timestamp with time zone NOT NULL
 );
 
--- CREATE TYPE fileType AS ENUM ('experience');
--- CREATE TABLE public.photo (
---     id int SERIAL,
---     path varchar(100) NOT NULL,
---     type fileType NOT NULL
--- );
-
-
 CREATE TABLE public.question (
-    id integer NOT NULL,
+    id SERIAL NOT NULL,
     question text NOT NULL,
     --type enum('text', 'single option', 'multiple options') --
-    PRIMARY KEY(id)
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE public.answer (
-    id integer NOT NULL,
+    id SERIAL NOT NULL,
     questionId integer NOT NULL,
     answer text
 );
 
 CREATE TABLE public.answer_question (
-    id integer NOT NULL,
+    id SERIAL NOT NULL,
     questionId integer NOT NULL,
     answerId integer NOT NULL,
     customAnswer text,
-    userId integer NOT NULL
-    PRIMARY KEY(id)
+    userId integer NOT NULL PRIMARY KEY (id)
 );
 
 CREATE TABLE public.file (
-    id integer SERIAL NOT NULL,
+    id SERIAL NOT NULL,
     path character varying NOT NULL,
     createdAt date,
-    PRIMARY KEY(id)
+    PRIMARY KEY (id)
 );
 
 -- COPY public.user (id, username, email, pwd, address, roleId, createdAt, updatedAt, registrationStatus, privKey, transferBlockchainStatus) FROM stdin;
