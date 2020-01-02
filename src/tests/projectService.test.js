75e3b4d36bae96d858cc1cb66a4ca65a2e626454
Leonel Gobbi <lgobbi@atixlabs.com>
2020-01-02 17:28:22 +0000

    Apply suggestion to src/tests/projectService.test.js

--- src/tests/projectService.test.js
+++ src/tests/projectService.test.js
@@ -222,7 +222,7 @@ describe('Project Service Test', () => {
     });
   });
 
-  describe.only('Project thumbnail', () => {
+  describe('Project thumbnail', () => {
     beforeAll(() => {
       injectMocks(projectService, { projectDao, userDao });
     });
