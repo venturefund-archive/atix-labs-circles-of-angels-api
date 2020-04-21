5b6d0dc5b9b69b791e6cc8d7a2b2b41e4a73e742
lgobbi-atix <lgobbi@atixlabs.com>
2020-04-21 16:02:17 -0300

    fix: correct rsk explorer url for addresses
    Ref #1366

--- src/rest/services/helpers/txExplorerHelper.js
+++ src/rest/services/helpers/txExplorerHelper.js
@@ -3,7 +3,7 @@ const config = require('config');
 const { explorerLink } = config;
 const BLOCK_ROUTE = 'block';
 const TX_ROUTE = 'tx';
-const ADDRESSES_ROUTE = 'addresses';
+const ADDRESSES_ROUTE = 'address';
 
 exports.buildTxURL = txHash => `${explorerLink}/${TX_ROUTE}/${txHash}`;
 
