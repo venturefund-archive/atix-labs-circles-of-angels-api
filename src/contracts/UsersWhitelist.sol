b94f6f621d62d87a5b6070cc4ac2564ebc638087
cristian.atix <caballay@atixlabs.com>
2020-12-16 16:40:54 -0300

    fix: map error

--- src/contracts/UsersWhitelist.sol
+++ src/contracts/UsersWhitelist.sol
@@ -20,7 +20,7 @@ contract UsersWhitelist is Initializable, Ownable {
     }
 
     function removeUser(address _user) public onlyOwner {
-        delete items[_user];
+        delete users[_user];
         emit RemovedFromWhitelist(_user);
     }
 }
