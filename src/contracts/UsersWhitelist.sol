bf233e43d5010470d8d285b2b73404b0a1f56a21
cristian.atix <caballay@atixlabs.com>
2020-12-16 16:29:26 -0300

    feat: add contract

---
+++ src/contracts/UsersWhitelist.sol
@@ -0,0 +1,26 @@
+pragma solidity ^0.5.8;
+
+import '@openzeppelin/upgrades/contracts/Initializable.sol';
+import '@openzeppelin/contracts-ethereum-package/contracts/ownership/Ownable.sol';
+
+contract UsersWhitelist is Initializable, Ownable {
+
+    mapping (address => bool) public users;
+
+    event AddedToWhitelist(address indexed account);
+    event RemovedFromWhitelist(address indexed account);
+
+    function initialize() public initialized {
+        Ownable.initialize(msg.sender);
+    }
+
+    function addUser(address _user) public onlyOwner {
+        users[_user] = true;
+        emit AddedToWhitelist(_user);
+    }
+
+    function removeUser(address _user) public onlyOwner {
+        delete items[_user];
+        emit RemovedFromWhitelist(_user);
+    }
+}
