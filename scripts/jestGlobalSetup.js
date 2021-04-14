b3d508f3310644511a714aa6a242d7101b844381
awolfsdorf <awolfsdorf@atixlabs.com>
2021-04-14 11:41:46 -0300

    fix(tests): using sleep from bash

--- scripts/jestGlobalSetup.js
+++ scripts/jestGlobalSetup.js
@@ -1,15 +1,12 @@
-const { exec } = require('child_process');
+const { exec, execSync } = require('child_process');
 const { testConfig } = require('config');
 const { runGSN } = require('./runDevGsn');
 const logger = require('../src/rest/logger');
 
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
-
 async function runGanache() {
   exec('npm run ganache >> /dev/null');
-  await sleep(2000);
+  // Wait until ganache is up and running
+  execSync('sleep 3');
 }
 
 module.exports = async () => {
