bb905d36779140f9c954f3e39ac8674b260ac8bf
awolfsdorf <awolfsdorf@atixlabs.com>
2021-04-27 12:29:28 -0300

    feat: adding documentation for checkBalance and added to module exports

--- src/rest/services/balancesService.js
+++ src/rest/services/balancesService.js
@@ -70,6 +70,15 @@ async function _checkBalances(contracts, signer, web3, config) {
   }
 }
 
+/**
+ * @dev check if the balances the contract are below the threshold and
+ *  send tokens if its needed.
+ *
+ * @param recipient Recipient contract to fund if needed.
+ * @param signer Signer who sends the tokens
+ * @param web3 Web3 provider
+ * @param config Configuration of the recipient contract, with balanceThreshold and targetBalance
+ */
 async function checkBalance(recipient, signer, web3, config) {
   const contractBalance = BigNumber.from(await balance(web3, { recipient }));
   const balanceThreshold = parseEther(config.balanceThreshold);
@@ -99,5 +108,6 @@ async function checkBalance(recipient, signer, web3, config) {
 
 module.exports = {
   checkGSNAccountBalance,
-  checkContractBalances
+  checkContractBalances,
+  checkBalance
 };
